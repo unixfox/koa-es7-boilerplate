515150de793cf8bae7fa584be98b8648c72c4921
Emilien Devos <contact@emiliendevos.be>
2020-10-28 17:54:12 +0200

    still trying to make ESM support working

--- src/lib/container.js
+++ src/lib/container.js
@@ -1,21 +1,7 @@
-import { createContainer, Lifetime, InjectionMode, asValue } from 'awilix'
+import { createContainer, Lifetime, asValue, asClass, asFunction } from 'awilix'
 import { logger } from './logger.js'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-
-/**
- * Using Awilix, the following files and folders (glob patterns)
- * will be loaded.
- */
-const modulesToLoad = [
-  // Services should be scoped to the request.
-  // This means that each request gets a separate instance
-  // of a service.
-  ['services/*.js', Lifetime.SCOPED],
-  // Stores will be singleton (1 instance per process).
-  // This is just for demo purposes, you can do whatever you want.
-  ['stores/*.js', Lifetime.SINGLETON]
-]
+import TodoService from '../services/todo-service.js'
+import todoStore from '../stores/todo-store.js'
 
 /**
  * Configures a new container.
@@ -23,28 +9,10 @@ const modulesToLoad = [
  * @return {Object} The container.
  */
 export function configureContainer() {
-  const opts = {
-    // Classic means Awilix will look at function parameter
-    // names rather than passing a Proxy.
-    injectionMode: InjectionMode.CLASSIC,
-    require: async function(uri) {
-      const require = await import(uri)
-      return require
-    }
-  }
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = dirname(__filename)
-  return createContainer(opts)
-    .loadModules(modulesToLoad, {
-      // `modulesToLoad` paths should be relative
-      // to this file's parent directory.
-      cwd: `${__dirname}/..`,
-      // Example: registers `services/todo-service.js` as `todoService`
-      formatName: 'camelCase'
-    })
-    .register({
-      // Our logger is already constructed,
-      // so provide it as-is to anyone who wants it.
-      logger: asValue(logger)
-    })
+  const container = createContainer().register({
+    todoService: asClass(TodoService, { lifetime: Lifetime.SCOPED }),
+    todoStore: asFunction(todoStore, { lifetime: Lifetime.SINGLETON }),
+    logger: asValue(logger)
+  })
+  return container
 }
